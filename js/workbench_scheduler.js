b203dc78f38b0e16e45eb9487cf235eed673244d
davidraijmakers <davidraijmakers@3078069.no-reply.drupal.org>
2015-01-17 11:33:54 -0500

    Issue #2407075 by davidraijmakers: Improved js translation, translations for module and admin.inc added

--- js/workbench_scheduler.js
+++ js/workbench_scheduler.js
@@ -1,7 +1,7 @@
 /**
-* @file
-* Sets the summary for Workbench Scheduler on vertical tabs.
-*/
+ * @file
+ * Sets the summary for Workbench Scheduler on vertical tabs.
+ */
 (function ($) {
 
     // Hiding start and end date fields
@@ -60,19 +60,21 @@
                     // Start Date and Time
                     var start_date = $('input[name="workbench_scheduler_start_date[date]"]');
                     var start_time = $('input[name="workbench_scheduler_start_date[time]"]');
+                    var start_text = $('label[for="edit-workbench-scheduler-start-date"]');
                     if (start_date.val() || start_time.val()) {
-                        vals.push(Drupal.checkPlain(Drupal.t('Start Date: ') + start_date.val() + ' ' + start_time.val()));
+                        vals.push(Drupal.t('@start_text: @start_date @start_time', {'@start_text': start_text.text(), '@start_date':start_date.val(), '@start_time':start_time.val()}));
                     }
 
                     // End Date and Time
                     var end_date = $('input[name="workbench_scheduler_end_date[date]"]');
                     var end_time = $('input[name="workbench_scheduler_end_date[time]"]');
+                    var end_text = $('label[for="edit-workbench-scheduler-end-date"]');
                     if (end_date.val() || end_time.val()) {
-                        vals.push(Drupal.checkPlain(Drupal.t('End Date: ') + end_date.val() + ' ' + end_time.val()));
+                        vals.push(Drupal.t('@end_text: @end_date @end_time', {'@end_text': end_text.text(), '@end_date':end_date.val(), '@end_time':end_time.val()}));
                     }
                 }
 
-               return vals.join("<br />");
+                return vals.join("<br />");
 
             });
         }
