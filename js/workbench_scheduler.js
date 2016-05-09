0cbea1c7b9703c225e7ae450cd51ec2b3759d06f
partdigital <partdigital@1386490.no-reply.drupal.org>
2016-05-09 09:14:18 -0400

    Issue #2718023 by partdigital, mairi: Tableselect 'select all' checkbox doesn't fire change events on node edit form.

--- js/workbench_scheduler.js
+++ js/workbench_scheduler.js
@@ -7,6 +7,15 @@
 
 (function ($) {
 
+    // Select all table checkbox. 
+    Drupal.behaviors.workbenchSchedulerSettingsSelectAllToggle = {
+       attach: function (context) {
+           // Trigger change event on all checkboxes. 
+           $('table.workbench-scheduler-sid th.select-all > input.form-checkbox').change(function() {
+               $('input.workbench-scheduler-sid').change();
+           });
+       } 
+    }
     // Vertical tabs.
     Drupal.behaviors.workbenchSchedulerSettingsSummary = {
         attach: function (context) {
@@ -16,20 +25,22 @@
 
                 // Schedule type.
                 $('input.workbench-scheduler-sid:checked').each(function(){
-
                     var type_input = $(this);
                     var type = Drupal.settings.workbench_scheduler.schedules[type_input.val()];
 
                     // If schedule exists.
-                    if(type_input.val() > 0){
+                    if (type_input.val() > 0) {
 
                         // Date and Time.
-                        var date = $('input[name="workbench_scheduler_date['+type_input.val()+'][date]"]');
-                        var time = $('input[name="workbench_scheduler_date['+type_input.val()+'][time]"]');
+                        var date = $('input[name="workbench_scheduler_date[' + type_input.val() + '][date]"]');
+                        var time = $('input[name="workbench_scheduler_date[' + type_input.val() + '][time]"]');
 
                         // Reformat date to be easier to understand
                         if (date.val() || time.val()) {
-                            vals.push(type.label + ': ' + Drupal.t('@date @time', {'@date':date.val(), '@time':time.val()}));
+                            vals.push(type.label + ': ' + Drupal.t('@date @time', {
+                                    '@date': date.val(),
+                                    '@time': time.val()
+                                }));
                         }
                     }
                 });
