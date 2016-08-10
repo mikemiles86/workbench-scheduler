62839bceee2413de12ce936fdc17041bc022ee2a
mairi <mairi@2504044.no-reply.drupal.org>
2016-08-10 17:25:17 -0400

    Issue #2718725 by partdigital, mairi: Permission refinements to allow viewing of schedules and honour workbench moderation transition permissions

--- js/workbench_scheduler.js
+++ js/workbench_scheduler.js
@@ -21,30 +21,49 @@
         attach: function (context) {
 
             $('#edit-workbench-scheduler', context).drupalSetSummary(function (context) {
-                var vals = [];
+                // Create summary array.
+                var summary = [];
 
-                // Schedule type.
-                $('input.workbench-scheduler-sid:checked').each(function(){
-                    var type_input = $(this);
-                    var type = Drupal.settings.workbench_scheduler.schedules[type_input.val()];
-
-                    // If schedule exists.
-                    if (type_input.val() > 0) {
-
+                // Loop through set schedules.
+                var schedules = Drupal.settings.workbench_scheduler.schedules;
+                for (var index in schedules) {
+                    if (schedules.hasOwnProperty(index)) {
+                        var schedule_type = schedules[index];
                         // Date and Time.
-                        var date = $('input[name="workbench_scheduler_date[' + type_input.val() + '][date]"]');
-                        var time = $('input[name="workbench_scheduler_date[' + type_input.val() + '][time]"]');
+                        var date = $('input[name="workbench_scheduler_date['+schedule_type.sid+'][date]"]');
+                        var time = $('input[name="workbench_scheduler_date['+schedule_type.sid+'][time]"]');
 
                         // Reformat date to be easier to understand
                         if (date.val() || time.val()) {
-                            vals.push(type.label + ': ' + Drupal.t('@date @time', {
-                                    '@date': date.val(),
-                                    '@time': time.val()
-                                }));
+                            summary.push({
+                            'sid': schedule_type.sid,
+                            'label': schedule_type.label,
+                            'date': date.val(),
+                            'time': time.val()
+                        });
+
+                        }
+                    }
+                }
+                
+                // Remove any schedules that are unchecked.
+                $('.workbench-schedule-sid input[type="checkbox"]:not(:checked)').each(function(){
+                    var type_input = $(this);
+                    for (index in summary) {
+                        if (summary[index].sid == type_input.val()) {
+                            delete summary[index]
                         }
                     }
                 });
 
+                // Compile final summary.
+                var vals = [];
+                for (var index in summary) {
+                    if (summary.hasOwnProperty(index)) {
+                        var schedule = summary[index];
+                        vals.push(schedule.label + ': ' + Drupal.t('@date @time', {'@date':schedule.date, '@time':schedule.time}));
+                    }
+                }
                 return vals.join("<br />");
             });
         }
